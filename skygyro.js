958c8a0b2fd258a87f085769b99bb9383c796925
jiho6693 <56946702+jiho6693@users.noreply.github.com>
2023-04-25 21:02:35 -0400

    d

---
+++ skygyro.js
@@ -0,0 +1,119 @@
+import * as THREE from 'three';
+
+			import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+			import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+			import { Sky } from 'three/addons/objects/Sky.js';
+			import {DeviceOrientationControls} from './DeviceOrientationControls.js';
+
+			let camera, scene, renderer;
+
+			let sky, sun;
+
+			init();
+			render();
+			
+			
+
+			function initSky() {
+
+				// Add Sky
+				sky = new Sky();
+				sky.scale.setScalar( 450000 );
+				scene.add( sky );
+
+				sun = new THREE.Vector3();
+
+				/// GUI
+
+				const effectController = {
+					turbidity: 5.7,
+					rayleigh: 1.64,
+					mieCoefficient: 0.001,
+					mieDirectionalG: 0.988,
+					elevation: 0,
+					azimuth: 180,
+					exposure: renderer.toneMappingExposure
+				};
+
+				function guiChanged() {
+
+					const uniforms = sky.material.uniforms;
+					uniforms[ 'turbidity' ].value = effectController.turbidity;
+					uniforms[ 'rayleigh' ].value = effectController.rayleigh;
+					uniforms[ 'mieCoefficient' ].value = effectController.mieCoefficient;
+					uniforms[ 'mieDirectionalG' ].value = effectController.mieDirectionalG;
+
+					const phi = THREE.MathUtils.degToRad( 90 - effectController.elevation );
+					const theta = THREE.MathUtils.degToRad( effectController.azimuth );
+
+					sun.setFromSphericalCoords( 1, phi, theta );
+
+					uniforms[ 'sunPosition' ].value.copy( sun );
+
+					renderer.toneMappingExposure = effectController.exposure;
+					renderer.render( scene, camera );
+
+				}
+
+				const gui = new GUI();
+
+		
+				
+				
+				gui.add( effectController, 'elevation', -360, 360, 0.1 ).onChange( guiChanged );
+			
+
+				guiChanged();
+
+			}
+
+			function init() {
+
+				camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 100, 2000000 );
+				camera.position.set( 0, 100, 2000 );
+
+				scene = new THREE.Scene();
+
+				const helper = new THREE.GridHelper( 10000, 2, 0xffffff, 0xffffff );
+				// scene.add( helper );
+
+				renderer = new THREE.WebGLRenderer();
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				renderer.outputEncoding = THREE.sRGBEncoding;
+				renderer.toneMapping = THREE.ACESFilmicToneMapping;
+				renderer.toneMappingExposure = 0.24;
+				document.body.appendChild( renderer.domElement );
+
+				const controls = new DeviceOrientationControls( camera, renderer.domElement );
+				controls.addEventListener( 'change', render );
+				//controls.maxPolarAngle = Math.PI / 2;
+				controls.enableZoom = false;
+				controls.enablePan = false;
+
+				initSky();
+
+				window.addEventListener( 'resize', onWindowResize );
+
+			}
+
+			if (window.DeviceOrientationEvent) {
+				window.addEventListener('deviceorientation', function (eventData) {
+			
+			
+					var tiltX = Math.round(eventData.gamma * 2 );
+			
+			
+					var tiltY =  Math.round(eventData.beta * 2);
+			
+					deviceOrientationHandler(tiltX,tiltY);
+			
+				}, false);
+			}
+			
+
+			function render() {
+
+				renderer.render( scene, camera );
+
+			}
