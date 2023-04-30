8cd4e30e8618114d6b1fdfc977821f9cb9d70219
jiho6693 <56946702+jiho6693@users.noreply.github.com>
2023-04-29 23:32:24 -0400

    a

--- sky+gyro+time.js
+++ sky+gyro+time.js
@@ -43,6 +43,11 @@
 
 			function rainy() {
 				
+				let cloudPartices = [];
+				
+				const scene = new THREE.Scene();
+				scene.fog = new THREE.FogExp2(0x11111f, 0.002);
+
 				const starGeo = new THREE.BufferGeometry ()
 				const vertices = [];
 				for (let i = 0; i < 15000; i++) {
